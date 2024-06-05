f37625b1b744105c2335d0cf05c1453a827362e2
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-05 13:38:49 +0530

    added explanation code for scope

--- scope.js
+++ scope.js
@@ -28,3 +28,11 @@ boton()
 
 //40 and 22
 cotton()
+
+/*
+- Every function has its own execution context
+- Hence all functions declared inside the function will be only available for the function and can also contain the same variable as the global environment
+- This is possible as the engine looks at first at the current environment for the variable
+- If a variable is not present in the local context, then it goes onto the **lexical scope environment**
+	- The scope of the function depends on where it is defined rather than from where it is called
+*/
