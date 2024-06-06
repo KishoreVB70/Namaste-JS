c8377db25638a92ce3161de0d6ade99b4b7c141e
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-06 13:58:30 +0530

    added notes for let and const file

---
+++ letconst.js
@@ -0,0 +1,21 @@
+/*
+### Let and Const vs Var
+- Let and cost was introduced in [[ES6]]
+- Any variable which is not initialized, but only declared will have the type undefined
+- Var is not preferred for much use cases
+- Hoisting is the difference between let, const vs var
+	- Var is hoisted(Given Memory) in the global object
+	- Let and Const are hoisted in a separate space(**Script**) and they **cannot be accessed** before **initialization** -> result in **reference error**
+	- **Script** is a separate object in the scope which contains the let and const variables
+- Temporal Dead Zone - The time from which the variable is hoisted and it is assigned a value(initialized)
+- Whenever we access a variable in the TDZ, we get a **reference error** indicating that we cannot access the variable before initialization
+	- Instead of the not defined error we get for variables which are not allocated memory
+- **Re declaration** of variable with the same name can be done for var, but not for let and const
+- Const behaves the same as the let but is **immutable** - A value **must be assigned** on the declaration and cannot be done after
+- Different errors In js
+    - Reference error - Accessing a variable which is not available in the scope - **TDZ** and **not defined**
+    - Syntax Error - thrown out even **before** the engine starts **executing** the code
+	    - Re declaring a const or let variable which is **already declared**
+	    - Not initializing value to const
+    - Type Error - Re assigning the value to a const
+*/
