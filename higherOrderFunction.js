c5e074bcac6d933bf3f407f941b772810752dd6f
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-08 10:03:59 +0530

    used higher order function to calculate different calculation using same template

---
+++ higherOrderFunction.js
@@ -0,0 +1,24 @@
+// Calculate the circumference, area and diameter of circles
+const radius = [1,4,7,9];
+function area(radius) {
+    return Math.PI * radius * radius;
+}
+function diameter(radius) {
+    return 2 * radius;
+}
+function circumference(radius) {
+    return Math.PI * 2 * radius;
+}
+
+function calculate(fnc) {
+    let output = [];
+    for (let i = 0; i < radius.length; i++) {
+        output.push(fnc(radius[i]));
+    }
+    return output;
+}
+
+// Calculate the area
+console.log("Area is: ", calculate(area));
+console.log("Circumference is: ", calculate(circumference));
+console.log("diameter is: ", calculate(diameter));
