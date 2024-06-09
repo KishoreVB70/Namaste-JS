775778a4412195537c7e281e3975a58d34d82180
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-09 08:23:14 +0530

    added notes📝 for callback hell

--- hell-callbacks.js
+++ hell-callbacks.js
@@ -21,3 +21,21 @@ api.createOrder(cart, (cost) => {
         })
     }) 
 })
+
+/*
+##### Callback hell
+- To  understand promises, deeper understanding of callbacks is required
+- Call backs enable the **Async operations** in the single threaded language
+- If the execution of one function is dependent on another function, then the dependent function can be passes as callback to the former function
+- Image of callbacks after callbacks for API calls
+	- ![[Pasted image 20240609080843.png]]
+- The chaining of callbacks inside callbacks inside callbacks like the prior image is the callback hell
+- This kind of horizontal code is called the **pyramid of doom**
+2 Issues of callbacks
+1. Callback hell -> Code grows horizontally due to the **chaining** of callbacks and becomes **unreadable**
+2. Inversion of control -> providing the control of our code to external agents (API) or other code which we don't have control over
+	- Loose control of code during callbacks
+	- In even our case of the prior image, we gave the responsibility of calling our function to the **external agent** of the **API**
+	- We put in great **trust** on the code of the external agent
+	- Some issues -> function is not called, function is called twice
+*/
