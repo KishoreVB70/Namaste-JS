b71c3adc1665190e84a758a2a8f87c63a1f75f77
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-09 12:41:13 +0530

    Made promise chain for 3 functions each having their own resolve and reject and finally a single catch statement

--- createPromise.js
+++ createPromise.js
@@ -3,9 +3,9 @@ const isValid = cart => cart.length <= 5
 const cart = ["XM5", "Legion7i", "LBP"];
 const cart1 = ["XM5", "Legion7i", "LBP", "f", "p", "c"];
 
+console.log("start");
 function createOrder(cart) {
-    const promise = new Promise((resolve, reject) => {
-
+    return promise = new Promise((resolve, reject) => {
         // Reject
         if (!isValid(cart)) {
             // This type of error must be handled or else it will be a harsh error
@@ -13,7 +13,7 @@ function createOrder(cart) {
 
             // The error will be an uncaught error
             // Catch is needed to handle this
-            const error = new Error("cart has more than 5 elements");
+            const error = new Error("Error: cart has more than 5 elements");
             reject(error);
         }
 
@@ -23,14 +23,53 @@ function createOrder(cart) {
         const orderId = 1;
 
         // Fake delay
-        setTimeout(() => resolve({'orderId': orderId}), 2000);
+        setTimeout(() => resolve({'orderId': orderId, "cost": 15}), 2000);
     });
-
-    return promise;
 }
 
-const result = createOrder(cart1);
-// What is returned through resolve is available as paramter of the callback function
-result.then(obj => console.log(obj))
-// Catch the error and do something with it
-.catch(error => console.log(error));
+function makePayment(obj) {
+    return new Promise((resolve, reject) => {
+
+        if(obj.cost > 500) {
+            const error = new Error("Over priced");
+            reject(error);
+        }
+
+        setTimeout(() => resolve({'orderId': obj.orderId, 'cost': obj.cost}), 2000);
+    });
+}
+
+function updateDetails(obj) {
+    return promise = new Promise((resolve, reject) => {
+
+        if(obj.cost < 500) {
+            const error = new Error("Uh Huh severly priced");
+            reject(error);
+        }
+
+        setTimeout(() => resolve({
+            'orderId': obj.orderId,
+            'cost': obj.cost,
+            'days': 1
+        }), 2000);
+    });
+}
+
+// For every then chain, whatever variable we need, we need to return it to the next level
+createOrder(cart)
+    .then(obj =>  {
+        console.log(`Your Order id: ${obj.orderId} and cost is: ${obj.cost}`);
+        // Returning the promise to the next level
+        return makePayment(obj);
+    })
+    .then(obj => {
+        console.log("cost is", obj.cost);
+        return updateDetails(obj);
+    })
+    .then(obj => console.log("Number of days: ", obj.days))
+    // Catch the error and do something with it
+    .catch(error => console.log(error.message));
+
+// The code in the mean time
+console.log("This is the good times");
+
