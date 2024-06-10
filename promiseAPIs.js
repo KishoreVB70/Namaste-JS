f3a0f92559a9caae3e3a91e4561335ad7834ad58
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-10 15:43:29 +0530

    added notes📝 for promiseAPIs

--- promiseAPIs.js
+++ promiseAPIs.js
@@ -118,3 +118,41 @@ fany();
 
 
 console.log("End");
+
+
+/*
+##### Promise API's
+- 4 API's
+1. `Promise.all`
+	- Used for parallel API calls
+		- Eg -> get 10 user id parallelly
+	- Handle multiple promises together
+		- Takes in array of promise
+	- `promise.all([p1,p2,p3])`
+	- The final result will be available only when all the 3 promises are resolved
+	- So, the time of `promise.all` depends on the longest API call in the array
+	- Output is the array of all the resolved values
+	- Failure case
+		- If once promise, get rejected, then **immediately** `promise.all` would return the error and will suspend the other promises
+2. `Promise.allSetteled`
+	- Similar to `Promise.all`
+	- But instead of returning total failure for failing for a single promise,
+	- Even if one promise is rejected, it will run the other promises and return the result of each promise
+	- Output -> `[output1, error, output3, output4, error`
+	- The output would be an array of objects
+	- Each objects would contain 2 values
+		1. status -> fulfilled or rejected
+		2. value -> resolved value
+		3. reason -> error returned by reject
+3. `Promise.race`
+	- Which ever promise gets resolved or rejected first,
+		- It will call `.then` or `.catch` respectively
+	- Which ever promise gets fulfilled or rejected will be the return value and will be executed **immediately**
+4. `Promise.any`
+	- Wait for the success of any of the promise
+	- It will ignore the failure of the promises
+	- It will return the first promise which is fulfilled
+	- Only when all the promises are failure, then it would return the failure
+	- The error would be an aggregate error -> An array containing all the errors
+	- If all promises are rejected, the **error message** is - all promises are rejected
+*/
