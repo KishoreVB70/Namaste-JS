6c5235e51bd5c7aeddb2c39803f0853902801ddc
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-11 10:24:14 +0530

    added code💻 for using call method to invoke a method of one object based on another object

--- this.js
+++ this.js
@@ -1,25 +1,29 @@
 "use strict";
-function norFunc() {
-    // This inside a function is actually undefined
-    // in non strict mode, it is substituted with the global object
-    // When strict mode is used, this inside function is undefined
-    // Strict -> undefined
-    // non strict -> global object
-    console.log(this);
+
+function differentValueOfThis() {
+    function norFunc() {
+        // This inside a function is actually undefined
+        // in non strict mode, it is substituted with the global object
+        // When strict mode is used, this inside function is undefined
+        // Strict -> undefined
+        // non strict -> global object
+        console.log(this);
+    }
+    
+    // Undefined in strict mode
+    norFunc();
+    
+    // the reference object(this) will be the this value inside of the function now
+    this.norFunc();
+    
+    const botFunc = () => {
+        console.log(this);
+    }
+    
+    // For an arrow function, even if in strict mode, it is the global object
+    botFunc();
 }
 
-// Undefined in strict mode
-// norFunc();
-
-// the reference object(this) will be the this value inside of the function now
-// this.norFunc();
-
-const botFunc = () => {
-    console.log(this);
-}
-
-// For an arrow function, even if in strict mode, it is the global object
-botFunc();
 
 // this inside an object
 
@@ -27,14 +31,21 @@ let user = {
     name: "John doe",
     age: 42,
     sex: "male",
-    meth: function() {
+    printAge: function() {
         // this points to the object itself
-        console.log("age is: ", this.age);
+        console.log(`Age of ${this.name} is ${this.age}`);
     },
     methBot: () => {
         // this points to the global object for arrow function
-        console.log("this is: ", this);
-    }
-}
+        console.log("this is: ", this.age);
+    },
+};
 
-user.meth();
+let user2 = {
+    name: "Preethiga",
+    age: 24
+};
+user.printAge();
+
+// This cannot be done for arrow functions
+user.printAge.call(user2);
