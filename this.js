e2507b1a0abfcd12f5cbbc3c7410cfaa40287a9f
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-11 10:56:51 +0530

    added a normal function inside arrow function to understand behavior

--- this.js
+++ this.js
@@ -20,13 +20,13 @@ function differentValueOfThis() {
         console.log(this);
     }
     
-    // For an arrow function, even if in strict mode, it is the global object
+    // For an arrow function, even if in strict mode, it is the lexical parent's this,
+    // Global object in this context
     botFunc();
 }
 
 
 // this inside an object
-
 let user = {
     name: "John doe",
     age: 42,
@@ -34,10 +34,25 @@ let user = {
     printAge: function() {
         // this points to the object itself
         console.log(`Age of ${this.name} is ${this.age}`);
+
+        // Arrow function take up this of their lexical parent
+        const func = () => {
+            console.log("Name from inner arrow function: ", this.name);
+        }
+        func();
     },
     methBot: () => {
         // this points to the global object for arrow function
-        console.log("this is: ", this.age);
+        function bot() {
+            // The this variable here is going to be undefined
+            // It is because, in strict mode, functions are always undefined
+
+            // If not in strict mode, then it is going to be the global object
+            // Not the outer object
+            console.log("This from the inner function: ", this);
+        }
+        bot();
+        console.log("this is: ", this);
     },
 };
 
@@ -45,7 +60,9 @@ let user2 = {
     name: "Preethiga",
     age: 24
 };
+
 user.printAge();
+user.methBot();
 
 // This cannot be done for arrow functions
 user.printAge.call(user2);
